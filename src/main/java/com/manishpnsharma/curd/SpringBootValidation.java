4118c3a54161755932197be39391ff65b1851fea
Manish Sharma <Manish.pnsharma@gmail.com>
2025-05-22 19:26:30 +0530

    first commit for validation

--- src/main/java/com/manishpnsharma/curd/SpringBootValidation.java
+++ src/main/java/com/manishpnsharma/curd/SpringBootValidation.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurdOperationApplication {
+public class SpringBootValidation {
     public static void main(String[] args) {
-        SpringApplication.run(CurdOperationApplication.class, args);
+        SpringApplication.run(SpringBootValidation.class, args);
     }
 }
